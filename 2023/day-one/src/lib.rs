4e7cc7fb6d5ff1eb3580ecf44894ba8ce59bfaef
Chad <chadrogers0010@gmail.com>
2024-11-01 20:49:06 -0700

    Change match_char_number() to boolean

--- 2023/day-one/src/lib.rs
+++ 2023/day-one/src/lib.rs
@@ -9,8 +9,15 @@ mod tests {}
 
 pub fn solver(strings: Vec<String>) -> i32 {
     let mut results_vec: Vec<(char, char)> = Vec::with_capacity(strings.len());
-    for each in strings {}
-    results_vec.iter().map(|a,b| )
+    strings.iter().map(|f| -> (char, char) {
+        let a: Option<i32> = None;
+        let b: Option<i32> = None;
+        for char in f.chars(){if match_char_number(char)}
+    });
+    results_vec
+        .iter()
+        .map(|f| -> i32 { format!("{}{}", f.0, f.1).parse().unwrap() })
+        .sum()
 }
 
 pub fn read_file<P: AsRef<Path>>(path: P) -> Result<Vec<String>, std::io::Error> {
@@ -20,18 +27,18 @@ pub fn read_file<P: AsRef<Path>>(path: P) -> Result<Vec<String>, std::io::Error>
     Ok(string_vec)
 }
 
-pub fn match_char_number(char: char) -> Option<char> {
+pub fn match_char_number(char: char) -> bool {
     match char {
-        char if char == '0' => Some('0'),
-        char if char == '1' => Some('1'),
-        char if char == '2' => Some('2'),
-        char if char == '3' => Some('3'),
-        char if char == '4' => Some('4'),
-        char if char == '5' => Some('5'),
-        char if char == '6' => Some('6'),
-        char if char == '7' => Some('7'),
-        char if char == '8' => Some('8'),
-        char if char == '9' => Some('9'),
-        _ => None,
+        char if char == '0' => true,
+        char if char == '1' => true,
+        char if char == '2' => true,
+        char if char == '3' => true,
+        char if char == '4' => true,
+        char if char == '5' => true,
+        char if char == '6' => true,
+        char if char == '7' => true,
+        char if char == '8' => true,
+        char if char == '9' => true,
+        _ => false,
     }
 }
