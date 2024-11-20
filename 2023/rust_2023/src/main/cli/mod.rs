071276ad75e06a2fcb07be16bb36c5f1c6e78605
Chad <chadrogers0010@gmail.com>
2024-11-19 18:02:12 -0800

    Add Day Branching

--- 2023/rust_2023/src/main/cli/mod.rs
+++ 2023/rust_2023/src/main/cli/mod.rs
@@ -1,6 +1,137 @@
 use clap::Command;
-mod const_file;
+pub mod const_file;
 
-pub fn build_cli() -> Command {
+pub fn build_cli() -> clap::Command {
     Command::new("Advent_of_Code")
+        .command_01()
+        .command_02()
+        .command_03()
+        .command_04()
+        .command_05()
+        .command_06()
+        .command_07()
+        .command_08()
+        .command_09()
+        .command_10()
+        .command_11()
+        .command_12()
+        .command_13()
+        .command_14()
+        .command_15()
+        .command_16()
+        .command_17()
+        .command_18()
+        .command_19()
+        .command_20()
+        .command_21()
+        .command_22()
+        .command_23()
+        .command_24()
+        .command_25()
+}
+
+trait ExtendCommand {
+    fn command_01(self) -> Command;
+    fn command_02(self) -> Command;
+    fn command_03(self) -> Command;
+    fn command_04(self) -> Command;
+    fn command_05(self) -> Command;
+    fn command_06(self) -> Command;
+    fn command_07(self) -> Command;
+    fn command_08(self) -> Command;
+    fn command_09(self) -> Command;
+    fn command_10(self) -> Command;
+    fn command_11(self) -> Command;
+    fn command_12(self) -> Command;
+    fn command_13(self) -> Command;
+    fn command_14(self) -> Command;
+    fn command_15(self) -> Command;
+    fn command_16(self) -> Command;
+    fn command_17(self) -> Command;
+    fn command_18(self) -> Command;
+    fn command_19(self) -> Command;
+    fn command_20(self) -> Command;
+    fn command_21(self) -> Command;
+    fn command_22(self) -> Command;
+    fn command_23(self) -> Command;
+    fn command_24(self) -> Command;
+    fn command_25(self) -> Command;
+}
+
+impl ExtendCommand for Command {
+    fn command_01(self) -> Command {
+        self
+    }
+    fn command_02(self) -> Command {
+        self
+    }
+    fn command_03(self) -> Command {
+        self
+    }
+    fn command_04(self) -> Command {
+        self
+    }
+    fn command_05(self) -> Command {
+        self
+    }
+    fn command_06(self) -> Command {
+        self
+    }
+    fn command_07(self) -> Command {
+        self
+    }
+    fn command_08(self) -> Command {
+        self
+    }
+    fn command_09(self) -> Command {
+        self
+    }
+    fn command_10(self) -> Command {
+        self
+    }
+    fn command_11(self) -> Command {
+        self
+    }
+    fn command_12(self) -> Command {
+        self
+    }
+    fn command_13(self) -> Command {
+        self
+    }
+    fn command_14(self) -> Command {
+        self
+    }
+    fn command_15(self) -> Command {
+        self
+    }
+    fn command_16(self) -> Command {
+        self
+    }
+    fn command_17(self) -> Command {
+        self
+    }
+    fn command_18(self) -> Command {
+        self
+    }
+    fn command_19(self) -> Command {
+        self
+    }
+    fn command_20(self) -> Command {
+        self
+    }
+    fn command_21(self) -> Command {
+        self
+    }
+    fn command_22(self) -> Command {
+        self
+    }
+    fn command_23(self) -> Command {
+        self
+    }
+    fn command_24(self) -> Command {
+        self
+    }
+    fn command_25(self) -> Command {
+        self
+    }
 }
--- 2023/rust_2023/src/main/main.rs
+++ 2023/rust_2023/src/main/main.rs
@@ -4,10 +4,91 @@ use std::{
     path::Path,
 };
 
+use cli::const_file::*;
+
 mod cli;
 
 fn main() {
-    let _args = cli::build_cli();
+    let arg_matches = cli::build_cli().get_matches();
+
+    match &arg_matches {
+        day_command if day_command.subcommand_matches(DAY_01).is_some() => {
+            println!("{} subcommand passed!", DAY_01);
+        }
+        day_command if day_command.subcommand_matches(DAY_02).is_some() => {
+            println!("{} subcommand passed!", DAY_02);
+        }
+        day_command if day_command.subcommand_matches(DAY_03).is_some() => {
+            println!("{} subcommand passed!", DAY_03);
+        }
+        day_command if day_command.subcommand_matches(DAY_04).is_some() => {
+            println!("{} subcommand passed!", DAY_04);
+        }
+        day_command if day_command.subcommand_matches(DAY_05).is_some() => {
+            println!("{} subcommand passed!", DAY_05);
+        }
+        day_command if day_command.subcommand_matches(DAY_06).is_some() => {
+            println!("{} subcommand passed!", DAY_06);
+        }
+        day_command if day_command.subcommand_matches(DAY_07).is_some() => {
+            println!("{} subcommand passed!", DAY_07);
+        }
+        day_command if day_command.subcommand_matches(DAY_08).is_some() => {
+            println!("{} subcommand passed!", DAY_08);
+        }
+        day_command if day_command.subcommand_matches(DAY_09).is_some() => {
+            println!("{} subcommand passed!", DAY_09);
+        }
+        day_command if day_command.subcommand_matches(DAY_10).is_some() => {
+            println!("{} subcommand passed!", DAY_10);
+        }
+        day_command if day_command.subcommand_matches(DAY_11).is_some() => {
+            println!("{} subcommand passed!", DAY_11);
+        }
+        day_command if day_command.subcommand_matches(DAY_12).is_some() => {
+            println!("{} subcommand passed!", DAY_12);
+        }
+        day_command if day_command.subcommand_matches(DAY_13).is_some() => {
+            println!("{} subcommand passed!", DAY_13);
+        }
+        day_command if day_command.subcommand_matches(DAY_14).is_some() => {
+            println!("{} subcommand passed!", DAY_14);
+        }
+        day_command if day_command.subcommand_matches(DAY_15).is_some() => {
+            println!("{} subcommand passed!", DAY_15);
+        }
+        day_command if day_command.subcommand_matches(DAY_16).is_some() => {
+            println!("{} subcommand passed!", DAY_16);
+        }
+        day_command if day_command.subcommand_matches(DAY_17).is_some() => {
+            println!("{} subcommand passed!", DAY_17);
+        }
+        day_command if day_command.subcommand_matches(DAY_18).is_some() => {
+            println!("{} subcommand passed!", DAY_18);
+        }
+        day_command if day_command.subcommand_matches(DAY_19).is_some() => {
+            println!("{} subcommand passed!", DAY_19);
+        }
+        day_command if day_command.subcommand_matches(DAY_20).is_some() => {
+            println!("{} subcommand passed!", DAY_20);
+        }
+        day_command if day_command.subcommand_matches(DAY_21).is_some() => {
+            println!("{} subcommand passed!", DAY_21);
+        }
+        day_command if day_command.subcommand_matches(DAY_22).is_some() => {
+            println!("{} subcommand passed!", DAY_22);
+        }
+        day_command if day_command.subcommand_matches(DAY_23).is_some() => {
+            println!("{} subcommand passed!", DAY_23);
+        }
+        day_command if day_command.subcommand_matches(DAY_24).is_some() => {
+            println!("{} subcommand passed!", DAY_24);
+        }
+        day_command if day_command.subcommand_matches(DAY_25).is_some() => {
+            println!("{} subcommand passed!", DAY_25);
+        }
+        _ => println!("No commands given"),
+    };
     println!("Hello AoC 2023!");
 }
 
