38e16b6b418802e937b76001f81459280b0069cf
Chad <chadrogers0010@gmail.com>
2024-11-19 16:34:38 -0800

    Fix */target Match

---
+++ 2023/day-one/target/debug/deps/day_one-ea2e8725149652ef.d
@@ -0,0 +1,5 @@
+C:\Users\brand\Chads-Advent-of-Code\2023\day-one\target\debug\deps\libday_one-ea2e8725149652ef.rmeta: src\lib.rs
+
+C:\Users\brand\Chads-Advent-of-Code\2023\day-one\target\debug\deps\day_one-ea2e8725149652ef.d: src\lib.rs
+
+src\lib.rs:
