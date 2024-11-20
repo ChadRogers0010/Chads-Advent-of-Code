38588783d2a279884fe2b160001c049f731f7c17
Chad <chadrogers0010@gmail.com>
2024-11-19 19:37:34 -0800

    Create Part Enum

--- 2023/rust_2023/src/lib/utils.rs
+++ 2023/rust_2023/src/lib/utils.rs
@@ -3,9 +3,16 @@ use std::{
     io::{BufRead, BufReader},
     path::Path,
 };
+
 pub fn read_file<P: AsRef<Path>>(path: P) -> Result<Vec<String>, std::io::Error> {
     let file = File::open(path)?;
     let bufreader = BufReader::new(file).lines();
     let string_vec: Vec<String> = bufreader.map(|f| f.unwrap()).collect();
     Ok(string_vec)
+}
+
+pub enum Part {
+    One,
+    Two,
+    Both,
 }
