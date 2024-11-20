2e9337cdd70312f6e812a7201196f3f61b942003
Chad <chadrogers0010@gmail.com>
2024-11-19 21:12:35 -0800

    Fix part_one_solver() Unwraping

--- 2023/rust_2023/src/lib/solutions/day_one.rs
+++ 2023/rust_2023/src/lib/solutions/day_one.rs
@@ -1,4 +1,14 @@
-pub fn part_one_solver(strings: &[String]) -> i32 {
+pub fn day_one_solver(part: crate::utils::Part) {
+    let input = crate::utils::read_file("../input/input_01.txt").unwrap();
+    let answer = match part {
+        crate::utils::Part::One => format!("{}", part_one_solver(&input)),
+        crate::utils::Part::Two => format!("{}", part_Two_solver(&input)),
+        crate::utils::Part::Both => todo!(),
+    };
+    println!("day_01:\n{}", answer)
+}
+
+fn part_one_solver(strings: &[String]) -> i32 {
     strings
         .iter()
         .map(|f| -> (char, char) {
@@ -16,17 +26,16 @@ pub fn part_one_solver(strings: &[String]) -> i32 {
                     break;
                 }
             }
-            (a.unwrap(), b.unwrap())
+            match (a, b) {
+                (Some(a), Some(b)) => (a, b),
+                _ => ('0', '0'),
+            }
         })
         .map(|f| -> i32 { format!("{}{}", f.0, f.1).parse().unwrap() })
         .sum()
 }
 
-pub fn part_two_solver(_input: &[String]) -> i32 {
-    todo!()
-}
-
-pub fn match_char_number(char: char) -> bool {
+fn match_char_number(char: char) -> bool {
     match char {
         char if char == '0' => true,
         char if char == '1' => true,
@@ -40,4 +49,8 @@ pub fn match_char_number(char: char) -> bool {
         char if char == '9' => true,
         _ => false,
     }
+}
+
+fn part_two_solver(_input: &[String]) -> i32 {
+    todo!()
 }
