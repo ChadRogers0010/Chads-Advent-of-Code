7c40731923b5744041883a13ce73fb14f5c012ff
Chad <chadrogers0010@gmail.com>
2024-11-19 21:08:47 -0800

    Fix/Broke/Fix again read_file()

--- 2023/rust_2023/src/lib/utils.rs
+++ 2023/rust_2023/src/lib/utils.rs
@@ -7,9 +7,14 @@ use std::{
 pub fn read_file<P: AsRef<Path>>(path: P) -> Result<Vec<String>, std::io::Error> {
     let file = File::open(path)?;
     let bufreader = BufReader::new(file).lines();
-    let string_vec: Vec<String> = bufreader.map(|f| f.unwrap()).collect();
+    let string_vec: Vec<String> = bufreader.map(|f| f.expect("Buffer failed")).collect();
+    // println!("Input: {:?}", string_vec);
     Ok(string_vec)
 }
+
+// pub fn find_input<P: AsRef<Path>>() -> P {
+//     let cur_loc = fs::ReadDir::from("./");
+// }
 
 pub enum Part {
     One,
