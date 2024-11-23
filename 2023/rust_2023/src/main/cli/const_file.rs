177b613469416c2fd1e8da07c6dc05d3c5ce2e8e
Chad <chadrogers0010@gmail.com>
2024-11-22 17:58:29 -0800

    Change cli builder to loop constructor

--- 2023/rust_2023/src/main/cli/const_file.rs
+++ 2023/rust_2023/src/main/cli/const_file.rs
@@ -28,3 +28,23 @@ pub const DAY_22: &'static str = "day-22";
 pub const DAY_23: &'static str = "day-23";
 pub const DAY_24: &'static str = "day-24";
 pub const DAY_25: &'static str = "day-25";
+pub const DAY_ARRAY: [&'static str; 25] = [
+    DAY_01, DAY_02, DAY_03, DAY_04, DAY_05, DAY_06, DAY_07, DAY_08, DAY_09, DAY_10, DAY_11, DAY_12,
+    DAY_13, DAY_14, DAY_15, DAY_16, DAY_17, DAY_18, DAY_19, DAY_20, DAY_21, DAY_22, DAY_23, DAY_24,
+    DAY_25,
+];
+
+pub const YEAR_2015: &'static str = "year-2015";
+pub const YEAR_2016: &'static str = "year-2016";
+pub const YEAR_2017: &'static str = "year-2017";
+pub const YEAR_2018: &'static str = "year-2018";
+pub const YEAR_2019: &'static str = "year-2019";
+pub const YEAR_2020: &'static str = "year-2020";
+pub const YEAR_2021: &'static str = "year-2021";
+pub const YEAR_2022: &'static str = "year-2022";
+pub const YEAR_2023: &'static str = "year-2023";
+pub const YEAR_2024: &'static str = "year-2024";
+pub const YEAR_ARRAY: [&'static str; 10] = [
+    YEAR_2015, YEAR_2016, YEAR_2017, YEAR_2018, YEAR_2019, YEAR_2020, YEAR_2021, YEAR_2022,
+    YEAR_2023, YEAR_2024,
+];
--- 2023/rust_2023/src/main/cli/mod.rs
+++ 2023/rust_2023/src/main/cli/mod.rs
@@ -1,155 +1,31 @@
 use clap::Command;
 use clap::*;
-
 use const_file::*;
 pub mod const_file;
-pub fn build_cli() -> clap::Command {
-    Command::new(PROGRAM_NAME)
-        // .short_flag('o')
-        // .short_flag('t')
-        // .short_flag('b')
-        .arg(Arg::new(PART_ONE).short('o').long("part-one").alias("one"))
-        .arg(Arg::new(PART_TWO).short('t').long("part-two").alias("two"))
-        .arg(
-            Arg::new(PART_BOTH)
-                .short('b')
-                .long("part-both")
-                .alias("both"),
-        )
-        // .args([arg!(-o - -one), arg!(-t - -two), arg!(-b - -both)])
-        .command_01()
-        .command_02()
-        .command_03()
-        .command_04()
-        .command_05()
-        .command_06()
-        .command_07()
-        .command_08()
-        .command_09()
-        .command_10()
-        .command_11()
-        .command_12()
-        .command_13()
-        .command_14()
-        .command_15()
-        .command_16()
-        .command_17()
-        .command_18()
-        .command_19()
-        .command_20()
-        .command_21()
-        .command_22()
-        .command_23()
-        .command_24()
-        .command_25()
+pub fn build_cli() -> Command {
+    Command::new(PROGRAM_NAME).init_years()
 }
 
 trait ExtendCommand {
-    fn command_01(self) -> Command;
-    fn command_02(self) -> Command;
-    fn command_03(self) -> Command;
-    fn command_04(self) -> Command;
-    fn command_05(self) -> Command;
-    fn command_06(self) -> Command;
-    fn command_07(self) -> Command;
-    fn command_08(self) -> Command;
-    fn command_09(self) -> Command;
-    fn command_10(self) -> Command;
-    fn command_11(self) -> Command;
-    fn command_12(self) -> Command;
-    fn command_13(self) -> Command;
-    fn command_14(self) -> Command;
-    fn command_15(self) -> Command;
-    fn command_16(self) -> Command;
-    fn command_17(self) -> Command;
-    fn command_18(self) -> Command;
-    fn command_19(self) -> Command;
-    fn command_20(self) -> Command;
-    fn command_21(self) -> Command;
-    fn command_22(self) -> Command;
-    fn command_23(self) -> Command;
-    fn command_24(self) -> Command;
-    fn command_25(self) -> Command;
+    fn init_years(self) -> Self;
+    fn init_days(self) -> Self;
 }
 
-fn init_command(name: &'static str) -> Command {
-    Command::new(name)
-}
+impl ExtendCommand for clap::Command {
+    fn init_years(self) -> Self {
+        let mut scratch = self;
+        for year in YEAR_ARRAY {
+            let subcom = clap::Command::new(year).init_days().about(year);
+            scratch = scratch.subcommand(subcom);
+        }
+        scratch
+    }
 
-impl ExtendCommand for Command {
-    fn command_01(self) -> Command {
-        self.subcommand(init_command(DAY_01))
-    }
-    fn command_02(self) -> Command {
-        self.subcommand(init_command(DAY_02))
-    }
-    fn command_03(self) -> Command {
-        self.subcommand(init_command(DAY_03))
-    }
-    fn command_04(self) -> Command {
-        self.subcommand(init_command(DAY_04))
-    }
-    fn command_05(self) -> Command {
-        self.subcommand(init_command(DAY_05))
-    }
-    fn command_06(self) -> Command {
-        self.subcommand(init_command(DAY_06))
-    }
-    fn command_07(self) -> Command {
-        self.subcommand(init_command(DAY_07))
-    }
-    fn command_08(self) -> Command {
-        self.subcommand(init_command(DAY_08))
-    }
-    fn command_09(self) -> Command {
-        self.subcommand(init_command(DAY_09))
-    }
-    fn command_10(self) -> Command {
-        self.subcommand(init_command(DAY_10))
-    }
-    fn command_11(self) -> Command {
-        self.subcommand(init_command(DAY_11))
-    }
-    fn command_12(self) -> Command {
-        self.subcommand(init_command(DAY_12))
-    }
-    fn command_13(self) -> Command {
-        self.subcommand(init_command(DAY_13))
-    }
-    fn command_14(self) -> Command {
-        self.subcommand(init_command(DAY_14))
-    }
-    fn command_15(self) -> Command {
-        self.subcommand(init_command(DAY_15))
-    }
-    fn command_16(self) -> Command {
-        self.subcommand(init_command(DAY_16))
-    }
-    fn command_17(self) -> Command {
-        self.subcommand(init_command(DAY_17))
-    }
-    fn command_18(self) -> Command {
-        self.subcommand(init_command(DAY_18))
-    }
-    fn command_19(self) -> Command {
-        self.subcommand(init_command(DAY_19))
-    }
-    fn command_20(self) -> Command {
-        self.subcommand(init_command(DAY_20))
-    }
-    fn command_21(self) -> Command {
-        self.subcommand(init_command(DAY_21))
-    }
-    fn command_22(self) -> Command {
-        self.subcommand(init_command(DAY_22))
-    }
-    fn command_23(self) -> Command {
-        self.subcommand(init_command(DAY_23))
-    }
-    fn command_24(self) -> Command {
-        self.subcommand(init_command(DAY_24))
-    }
-    fn command_25(self) -> Command {
-        self.subcommand(init_command(DAY_25))
+    fn init_days(self) -> Self {
+        let mut scratch = self;
+        for day in DAY_ARRAY {
+            scratch = scratch.arg(Arg::new(day));
+        }
+        scratch
     }
 }
